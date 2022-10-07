e890504da0e9618017befd5cb5bd92e0c3631551
eak24 <eak24@users.noreply.github.com>
2022-10-07 00:14:40 +0000

    Generate bindings for Onshape 1.154.6797-6824f8df9946

--- onshape/model_bt_table_base_cross_highlight_data_2609.go
+++ onshape/model_bt_table_base_cross_highlight_data_2609.go
@@ -3,7 +3,7 @@ Onshape REST API
 
 The Onshape REST API consumed by all client. # Authorization The simplest way to authorize and enable the **Try it out** functionality is to sign in to Onshape and use the current session. The **Authorize** button enables other authorization techniques. To ensure the current session isn't used when trying other authentication techniques, make sure to remove the Onshape cookie as per the instructions for your particular browser. Alternatively, a private or incognito window may be used. Here's [how to remove a specific cookie on Chrome](https://support.google.com/chrome/answer/95647#zippy=%2Cdelete-cookies-from-a-site). - **Current Session** authorization is enabled by default if the browser is already signed in to [Onshape](/). - **OAuth2** authorization uses an Onshape OAuth2 app created on the [Onshape Developer Portal](https://dev-portal.onshape.com/oauthApps). The redirect URL field should include `https://cad.onshape.com/glassworks/explorer/oauth2-redirect.html`. - **API Key** authorization using basic authentication is also available. The keys can be generated in the [Onshape Developer Portal](https://dev-portal.onshape.com/keys). In the authentication dialog, enter the access key in the `Username` field, and enter the secret key in the `Password` field. Basic authentication should only be used during the development process since sharing API Keys provides the same level of access as a username and password.
 
-API version: 1.154.6779-364d99ceba76
+API version: 1.154.6797-6824f8df9946
 Contact: api-support@onshape.zendesk.com
 */
 
@@ -46,6 +46,56 @@ func NewBTTableBaseCrossHighlightData2609() *BTTableBaseCrossHighlightData2609 {
 func NewBTTableBaseCrossHighlightData2609WithDefaults() *BTTableBaseCrossHighlightData2609 {
 	this := BTTableBaseCrossHighlightData2609{Newbase_BTTableBaseCrossHighlightData2609WithDefaults()}
 	return &this
+}
+
+// GetBtType returns the BtType field value if set, zero value otherwise.
+func (o *BTTableBaseCrossHighlightData2609) GetBtType() string {
+	type getResult interface {
+		GetBtType() string
+	}
+
+	if tx, ok := o.GetActualInstance().(getResult); ok {
+		return tx.GetBtType()
+	} else {
+		var de string
+		return de
+	}
+}
+
+// GetBtTypeOk returns a tuple with the BtType field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *BTTableBaseCrossHighlightData2609) GetBtTypeOk() (*string, bool) {
+	type getResult interface {
+		GetBtTypeOk() (*string, bool)
+	}
+
+	if tx, ok := o.GetActualInstance().(getResult); ok {
+		return tx.GetBtTypeOk()
+	} else {
+		return nil, false
+	}
+}
+
+// HasBtType returns a boolean if a field has been set.
+func (o *BTTableBaseCrossHighlightData2609) HasBtType() bool {
+	type getResult interface {
+		HasBtType() bool
+	}
+
+	if tx, ok := o.GetActualInstance().(getResult); ok {
+		return tx.HasBtType()
+	} else {
+		return false
+	}
+}
+
+// SetBtType gets a reference to the given string and assigns it to the BtType field.
+func (o *BTTableBaseCrossHighlightData2609) SetBtType(v string) {
+	type getResult interface {
+		SetBtType(v string)
+	}
+
+	o.GetActualInstance().(getResult).SetBtType(v)
 }
 
 // Unmarshal JSON data into one of the pointers in the struct
@@ -152,6 +202,7 @@ func (v *NullableBTTableBaseCrossHighlightData2609) UnmarshalJSON(src []byte) er
 }
 
 type base_BTTableBaseCrossHighlightData2609 struct {
+	BtType *string `json:"btType,omitempty"`
 }
 
 // Newbase_BTTableBaseCrossHighlightData2609 instantiates a new base_BTTableBaseCrossHighlightData2609 object
@@ -171,7 +222,42 @@ func Newbase_BTTableBaseCrossHighlightData2609WithDefaults() *base_BTTableBaseCr
 	return &this
 }
 
+// GetBtType returns the BtType field value if set, zero value otherwise.
+func (o *base_BTTableBaseCrossHighlightData2609) GetBtType() string {
+	if o == nil || o.BtType == nil {
+		var ret string
+		return ret
+	}
+	return *o.BtType
+}
+
+// GetBtTypeOk returns a tuple with the BtType field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *base_BTTableBaseCrossHighlightData2609) GetBtTypeOk() (*string, bool) {
+	if o == nil || o.BtType == nil {
+		return nil, false
+	}
+	return o.BtType, true
+}
+
+// HasBtType returns a boolean if a field has been set.
+func (o *base_BTTableBaseCrossHighlightData2609) HasBtType() bool {
+	if o != nil && o.BtType != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetBtType gets a reference to the given string and assigns it to the BtType field.
+func (o *base_BTTableBaseCrossHighlightData2609) SetBtType(v string) {
+	o.BtType = &v
+}
+
 func (o base_BTTableBaseCrossHighlightData2609) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
+	if o.BtType != nil {
+		toSerialize["btType"] = o.BtType
+	}
 	return json.Marshal(toSerialize)
 }
