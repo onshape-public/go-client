8a4614ae9741bdd720158eaa1f53ed1b85e8c467
eak24 <eak24@users.noreply.github.com>
2022-10-15 00:11:27 +0000

    Generate bindings for Onshape 1.155.6922-11b87e04d539

--- onshape/docs/BTBasePartColorCycle2614.md
+++ onshape/docs/BTBasePartColorCycle2614.md
@@ -4,7 +4,6 @@
 
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
-**BtType** | Pointer to **string** |  | [optional] 
 
 ## Methods
 
@@ -24,31 +23,6 @@ will change when the set of required properties is changed
 NewBTBasePartColorCycle2614WithDefaults instantiates a new BTBasePartColorCycle2614 object
 This constructor will only assign default values to properties that have it defined,
 but it doesn't guarantee that properties required by API are set
-
-### GetBtType
-
-`func (o *BTBasePartColorCycle2614) GetBtType() string`
-
-GetBtType returns the BtType field if non-nil, zero value otherwise.
-
-### GetBtTypeOk
-
-`func (o *BTBasePartColorCycle2614) GetBtTypeOk() (*string, bool)`
-
-GetBtTypeOk returns a tuple with the BtType field if it's non-nil, zero value otherwise
-and a boolean to check if the value has been set.
-
-### SetBtType
-
-`func (o *BTBasePartColorCycle2614) SetBtType(v string)`
-
-SetBtType sets BtType field to given value.
-
-### HasBtType
-
-`func (o *BTBasePartColorCycle2614) HasBtType() bool`
-
-HasBtType returns a boolean if a field has been set.
 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- onshape/model_bt_entity_deletion_24.go
+++ onshape/model_bt_entity_deletion_24.go
@@ -3,7 +3,7 @@ Onshape REST API
 
 The Onshape REST API consumed by all client. # Authorization The simplest way to authorize and enable the **Try it out** functionality is to sign in to Onshape and use the current session. The **Authorize** button enables other authorization techniques. To ensure the current session isn't used when trying other authentication techniques, make sure to remove the Onshape cookie as per the instructions for your particular browser. Alternatively, a private or incognito window may be used. Here's [how to remove a specific cookie on Chrome](https://support.google.com/chrome/answer/95647#zippy=%2Cdelete-cookies-from-a-site). - **Current Session** authorization is enabled by default if the browser is already signed in to [Onshape](/). - **OAuth2** authorization uses an Onshape OAuth2 app created on the [Onshape Developer Portal](https://dev-portal.onshape.com/oauthApps). The redirect URL field should include `https://cad.onshape.com/glassworks/explorer/oauth2-redirect.html`. - **API Key** authorization using basic authentication is also available. The keys can be generated in the [Onshape Developer Portal](https://dev-portal.onshape.com/keys). In the authentication dialog, enter the access key in the `Username` field, and enter the secret key in the `Password` field. Basic authentication should only be used during the development process since sharing API Keys provides the same level of access as a username and password.
 
-API version: 1.155.6905-ae59ed040327
+API version: 1.155.6922-11b87e04d539
 Contact: api-support@onshape.zendesk.com
 */
 
@@ -17,13 +17,13 @@ import (
 
 // BTEntityDeletion24 struct for BTEntityDeletion24
 type BTEntityDeletion24 struct {
-	BtType              *string              `json:"btType,omitempty"`
 	CopyWithoutGeometry *BTBaseEntityData33  `json:"copyWithoutGeometry,omitempty"`
 	Decompressed        *BTBaseEntityData33  `json:"decompressed,omitempty"`
 	Deletion            *bool                `json:"deletion,omitempty"`
 	FeatureIds          []string             `json:"featureIds,omitempty"`
 	FromSketch          *bool                `json:"fromSketch,omitempty"`
 	Geometries          []BTEntityGeometry35 `json:"geometries,omitempty"`
+	BtType              *string              `json:"btType,omitempty"`
 }
 
 // NewBTEntityDeletion24 instantiates a new BTEntityDeletion24 object
@@ -41,38 +41,6 @@ func NewBTEntityDeletion24() *BTEntityDeletion24 {
 func NewBTEntityDeletion24WithDefaults() *BTEntityDeletion24 {
 	this := BTEntityDeletion24{}
 	return &this
-}
-
-// GetBtType returns the BtType field value if set, zero value otherwise.
-func (o *BTEntityDeletion24) GetBtType() string {
-	if o == nil || o.BtType == nil {
-		var ret string
-		return ret
-	}
-	return *o.BtType
-}
-
-// GetBtTypeOk returns a tuple with the BtType field value if set, nil otherwise
-// and a boolean to check if the value has been set.
-func (o *BTEntityDeletion24) GetBtTypeOk() (*string, bool) {
-	if o == nil || o.BtType == nil {
-		return nil, false
-	}
-	return o.BtType, true
-}
-
-// HasBtType returns a boolean if a field has been set.
-func (o *BTEntityDeletion24) HasBtType() bool {
-	if o != nil && o.BtType != nil {
-		return true
-	}
-
-	return false
-}
-
-// SetBtType gets a reference to the given string and assigns it to the BtType field.
-func (o *BTEntityDeletion24) SetBtType(v string) {
-	o.BtType = &v
 }
 
 // GetCopyWithoutGeometry returns the CopyWithoutGeometry field value if set, zero value otherwise.
@@ -267,11 +235,40 @@ func (o *BTEntityDeletion24) SetGeometries(v []BTEntityGeometry35) {
 	o.Geometries = v
 }
 
+// GetBtType returns the BtType field value if set, zero value otherwise.
+func (o *BTEntityDeletion24) GetBtType() string {
+	if o == nil || o.BtType == nil {
+		var ret string
+		return ret
+	}
+	return *o.BtType
+}
+
+// GetBtTypeOk returns a tuple with the BtType field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *BTEntityDeletion24) GetBtTypeOk() (*string, bool) {
+	if o == nil || o.BtType == nil {
+		return nil, false
+	}
+	return o.BtType, true
+}
+
+// HasBtType returns a boolean if a field has been set.
+func (o *BTEntityDeletion24) HasBtType() bool {
+	if o != nil && o.BtType != nil {
+		return true
+	}
+
+	return false
+}
+
+// SetBtType gets a reference to the given string and assigns it to the BtType field.
+func (o *BTEntityDeletion24) SetBtType(v string) {
+	o.BtType = &v
+}
+
 func (o BTEntityDeletion24) MarshalJSON() ([]byte, error) {
 	toSerialize := map[string]interface{}{}
-	if o.BtType != nil {
-		toSerialize["btType"] = o.BtType
-	}
 	if o.CopyWithoutGeometry != nil {
 		toSerialize["copyWithoutGeometry"] = o.CopyWithoutGeometry
 	}
@@ -289,6 +286,9 @@ func (o BTEntityDeletion24) MarshalJSON() ([]byte, error) {
 	}
 	if o.Geometries != nil {
 		toSerialize["geometries"] = o.Geometries
+	}
+	if o.BtType != nil {
+		toSerialize["btType"] = o.BtType
 	}
 	return json.Marshal(toSerialize)
 }
