0225c2da8a0fcc3f188d8b7f00278aae45106cef
Abdulrahman Afia <aafia@ptc.com>
2024-05-08 16:31:29 -0400

    added TestGetVariablesExecute

--- onshape_test/api_variables_test.go
+++ onshape_test/api_variables_test.go
@@ -1,6 +1,8 @@
 package onshape_test
 
 import (
+	"net/http"
+	"reflect"
 	"testing"
 
 	"github.com/onshape-public/go-client/onshape"
@@ -44,4 +46,24 @@ func TestVariablesAPI(t *testing.T) {
         Expect: Todo(),
     }.Execute()
     
+}
+
+func TestGetVariablesExecute(t *testing.T) {
+    InitializeTester[*onshape.VariablesApiService](t)
+    obj, r, err := Context()["client"].(*onshape.APIClient).VariablesApi.GetVariablesExecute(onshape.ApiGetVariablesRequest{})
+    // obj, r, err := Context()["client"].(*onshape.APIClient).VariablesApi.GetVariablesExecute(onshape.ApiGetVariablesRequest{})
+
+    // Check if the returned values are of the expected types
+    type objOnshape []onshape.BTVariableTableInfo 
+    if reflect.TypeOf(obj).Kind() != reflect.TypeOf(objOnshape{}).Kind()  {
+        t.Errorf("Expected an []BTVariableTableInfo, got %T", obj)
+    }
+
+    type httpRes http.Response
+    if reflect.TypeOf(r).Kind() != reflect.TypeOf(&httpRes{}).Kind()  {
+        t.Errorf("Expected a *http.Response, got %T", r)
+    }
+    if _, ok := err.(error); !ok {
+        t.Errorf("Expected a error, got %T", err)
+    }
 }
