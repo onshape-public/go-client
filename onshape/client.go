18c5ce5b7a5c91dd8b4cbe683d8cc836a7b2c886
knazarenko-ptc <59034535+knazarenko-ptc@users.noreply.github.com>
2025-07-25 00:12:05 +0000

    Generate bindings for Onshape 1.201.60357-92447bed4990

--- onshape/client.go
+++ onshape/client.go
@@ -40,7 +40,7 @@ var (
 	xmlCheck  = regexp.MustCompile(`(?i:(?:application|text)/xml)`)
 )
 
-// APIClient manages communication with the Onshape REST API API v1.201.59946-273cf652a478
+// APIClient manages communication with the Onshape REST API API v1.201.60357-92447bed4990
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *APIConfiguration
