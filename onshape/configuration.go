9ff7e42dc7411e800fbb6d86175bd8884f1fc8ec
knazarenko-ptc <59034535+knazarenko-ptc@users.noreply.github.com>
2025-09-25 00:10:59 +0000

    Generate bindings for Onshape 1.204.64141-17af267b1505

--- onshape/configuration.go
+++ onshape/configuration.go
@@ -101,7 +101,7 @@ type APIConfiguration struct {
 func NewAPIConfiguration() *APIConfiguration {
 	cfg := &APIConfiguration{
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "OpenAPI-Generator/1.204.64108-e1d66eef3e1e/go",
+		UserAgent:     "OpenAPI-Generator/1.204.64141-17af267b1505/go",
 		Debug:         false,
 		Servers: ServerConfigurations{
 			{
